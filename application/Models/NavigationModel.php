2eb550cff3186efa94653bbae1b1b9a5f1cad62d
Jack <guitarpoet@gmail.com>
2015-05-12 13:46:48 +0800

    Fixed the navigation bug.

--- application/Models/NavigationModel.php
+++ application/Models/NavigationModel.php
@@ -50,17 +50,19 @@ class NavigationModel extends Model implements LoggerAwareInterface, Initializab
 					continue;
 				}
 
-				$meta = \Clips\path_join($cur->getPath(), '__meta__.json');
+				$meta = \Clips\path_join($cur->getPath(), '__meta__.json'); // Try get the metadata from the __meta__.json file
 				if(file_exists($meta)) {
 					$currentAction = (array) \Clips\parse_json(file_get_contents($meta));
 					$currentAction['content'] = $name;
 					$currentAction['children'] = array();
+					$currentAction['path'] = $cur->getPathname();
 				}
 				else
-					$currentAction = array('label' => $name, 'type' => 'server', 'content' => $name, 'children' => array());
+					$currentAction = array('label' => $name, 'type' => 'server', 'content' => $name, 'children' => array(), 'path' => $cur->getPathname());
+
 				if($actionStack) {
 					$action = array_pop($actionStack);
-					if(strpos($name, $action['label']) !== false) {
+					if(dirname($currentAction['path'] == $action['path'])) {
 						$action['children'] []= $currentAction;
 						$actionStack []= $action;
 					}
@@ -78,19 +80,22 @@ class NavigationModel extends Model implements LoggerAwareInterface, Initializab
 				if($cur->getPath() == realpath($repo))
 					continue;
 				if($actionStack) {
-					if($cur->getExtension() != 'md')
+					if($cur->getExtension() != 'md') // We only want md file
 						continue;
 					$uri = substr($filename, strpos($filename, $repo) + strlen($repo) + 1);
-					$uri = 'doc/show/'.substr($uri, 0, strlen($uri) - 3);
+					$uri = 'doc/show/'.substr($uri, 0, strlen($uri) - 3); // Strip the .md extension
 					$name = $cur->getFileName();
 					$name = substr($name, 0, strlen($name) - 3);
 					$actionStack[count($actionStack) - 1]['children'] []= array('label' => $name, 'type' => 'server', 'content' => $uri);
-					print_r($actionStack);
 				}
 			}
 		}
 
-		print_r($data);
+		while($actionStack) {
+			$action = array_pop($actionStack);
+		}
+		$data []= $action;
+
 		$this->_action = new SimpleAction(array('children' => $data));
 	}
 
